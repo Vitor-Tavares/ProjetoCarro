9b35441b187a61a99282b60fa8de0a1cd3f46b17
Vitor-Tavares <vitortavaresifpb2017@gmail.com>
2017-08-02 09:18:14 -0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,20 @@
+import Carro
+def main():
+
+    carro1 = Carro.Carro("branca", "gol", 2006)
+    Carro.Carro.acelerar()
+    Carro.Carro.freiar()
+
+    Carro2 = Carro.Carro("azul", "fordk", 2003)
+    Carro.Carro.acelerar()
+    Carro.Carro.freiar()
+
+    Carro3 = Carro1
+
+
+
+
+
+
+if __name__=="__main__" :
+    main()
---
+++ Hibrido.py
@@ -0,0 +1,10 @@
+from Carro import Carro
+
+class hibrido(Carro)
+    def __init__ (self, cor, modelo, ano):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+
+    def carregarbateria(self):
+
